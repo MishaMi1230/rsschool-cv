415a926d06205caf45627c401a5b7fe0e6ecf41a
MishaMi1230 <miltomisha43@gmail.com>
2022-06-07 20:23:26 +0300

    feat: create a template and add a few scripts, add new button-открыть моё cv ;

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 console.log('Консоль пока пуста');
-document.querySelector('button').onclick = buttonclick;
+
+
+document.querySelector('.b-1').onclick = buttonclick1;
 let z = 1;
-function buttonclick() {
+function buttonclick1() {
     z = z + 1;
     if (z == 2){
     document.querySelector('h1').innerHTML = 'Тут думать надо';
@@ -10,5 +12,10 @@ function buttonclick() {
     } else {
     document.querySelector('h1').innerHTML = 'Сё, харе';   
     }
+}
 
+
+document.querySelector('.b-2').onclick = buttonclick2;
+function buttonclick2() {
+    document.location.href = 'https://mishami1230.github.io/rsschool-cv/cv';
 }
