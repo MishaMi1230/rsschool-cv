7a14eda2a2f6e3f9e8ed694bab383d535825ff08
MishaMi1230 <miltomisha43@gmail.com>
2022-06-07 21:02:11 +0300

    refactor: design

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let z = 1;
 function buttonclick1() {
     z = z + 1;
     if (z == 2){
-    document.querySelector('h1').innerHTML = 'Тут думать надо';
+    document.querySelector('h1').innerHTML = 'Тут думать надо!';
     } else if (z == 3){
     document.querySelector('h1').innerHTML = 'Забавная кнопка, правда?';
     } else {
-    document.querySelector('h1').innerHTML = 'Сё, харе';   
+    document.querySelector('h1').innerHTML = 'Сё, харе.';   
     }
 }
 
